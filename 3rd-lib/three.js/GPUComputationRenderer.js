7b011f756519f0ce778d9d18a24309309f35f047
odys-z <odysseusj@163.com>
2019-08-29 19:03:38 +0800

    ray casting mesh not rendered - vert pos not correct

--- 3rd-lib/three.js/GPUComputationRenderer.js
+++ 3rd-lib/three.js/GPUComputationRenderer.js
@@ -195,116 +195,73 @@ var GPUComputationRenderer = function ( sizeX, sizeY, renderer ) {
 			if ( variable.dependencies !== null ) {
 
 				for ( var d = 0; d < variable.dependencies.length; d ++ ) {
-
 					var depVar = variable.dependencies[ d ];
-
 					if ( depVar.name !== variable.name ) {
-
 						// Checks if variable exists
 						var found = false;
 						for ( var j = 0; j < this.variables.length; j ++ ) {
-
 							if ( depVar.name === this.variables[ j ].name ) {
-
 								found = true;
 								break;
-
 							}
-
 						}
-
 						if ( ! found ) {
-
 							return "Variable dependency not found. Variable=" + variable.name + ", dependency=" + depVar.name;
-
 						}
-
 					}
-
 					uniforms[ depVar.name ] = { value: null };
-
 					material.fragmentShader = "\nuniform sampler2D " + depVar.name + ";\n" + material.fragmentShader;
-
 				}
-
 			}
-
 		}
-
 		this.currentTextureIndex = 0;
-
 		return null;
-
 	};
 
 	this.compute = function () {
-
 		var currentTextureIndex = this.currentTextureIndex;
 		var nextTextureIndex = this.currentTextureIndex === 0 ? 1 : 0;
 
 		for ( var i = 0, il = this.variables.length; i < il; i ++ ) {
-
 			var variable = this.variables[ i ];
-
 			// Sets texture dependencies uniforms
 			if ( variable.dependencies !== null ) {
-
 				var uniforms = variable.material.uniforms;
 				for ( var d = 0, dl = variable.dependencies.length; d < dl; d ++ ) {
-
 					var depVar = variable.dependencies[ d ];
-
 					uniforms[ depVar.name ].value = depVar.renderTargets[ currentTextureIndex ].texture;
-
 				}
-
 			}
-
 			// Performs the computation for this variable
 			this.doRenderTarget( variable.material, variable.renderTargets[ nextTextureIndex ] );
-
 		}
 
 		this.currentTextureIndex = nextTextureIndex;
-
 	};
 
 	this.getCurrentRenderTarget = function ( variable ) {
-
 		return variable.renderTargets[ this.currentTextureIndex ];
-
 	};
 
 	this.getAlternateRenderTarget = function ( variable ) {
-
 		return variable.renderTargets[ this.currentTextureIndex === 0 ? 1 : 0 ];
-
 	};
 
 	function addResolutionDefine( materialShader ) {
-
 		materialShader.defines.resolution = 'vec2( ' + sizeX.toFixed( 1 ) + ', ' + sizeY.toFixed( 1 ) + " )";
-
 	}
 	this.addResolutionDefine = addResolutionDefine;
 
-
 	// The following functions can be used to compute things manually
-
 	function createShaderMaterial( computeFragmentShader, uniforms ) {
-
 		uniforms = uniforms || {};
-
 		var material = new THREE.ShaderMaterial( {
 			uniforms: uniforms,
 			vertexShader: getPassThroughVertexShader(),
 			fragmentShader: computeFragmentShader
 		} );
-
 		addResolutionDefine( material );
-
 		return material;
-
 	}
 
 	this.createShaderMaterial = createShaderMaterial;
@@ -344,46 +301,34 @@ var GPUComputationRenderer = function ( sizeX, sizeY, renderer ) {
 	};
 
 	this.renderTexture = function ( input, output ) {
-
 		// Takes a texture, and render out in rendertarget
 		// input = Texture
 		// output = RenderTarget
-
 		passThruUniforms.passThruTexture.value = input;
-
 		this.doRenderTarget( passThruShader, output );
-
 		passThruUniforms.passThruTexture.value = null;
-
 	};
 
 	this.doRenderTarget = function ( material, output ) {
-
 		var currentRenderTarget = renderer.getRenderTarget();
-
 		mesh.material = material;
 		renderer.setRenderTarget( output );
 		renderer.render( scene, camera );
 		mesh.material = passThruShader;
-
 		renderer.setRenderTarget( currentRenderTarget );
-
 	};
 
 	// Shaders
 
 	function getPassThroughVertexShader() {
-
 		return	"void main()	{\n" +
 				"\n" +
 				"	gl_Position = vec4( position, 1.0 );\n" +
 				"\n" +
 				"}\n";
-
 	}
 
 	function getPassThroughFragmentShader() {
-
 		return	"uniform sampler2D passThruTexture;\n" +
 				"\n" +
 				"void main() {\n" +
@@ -393,7 +338,6 @@ var GPUComputationRenderer = function ( sizeX, sizeY, renderer ) {
 				"	gl_FragColor = texture2D( passThruTexture, uv );\n" +
 				"\n" +
 				"}\n";
-
 	}
 
 };
